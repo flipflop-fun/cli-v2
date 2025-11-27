0618d33b39bf177eb6a77dcd110703317cdb6a9f
guqianfeng <guqianfeng@pro-m4.local>
2025-11-27 22:01:13 +0800

    flipflop-cli-v2 1.0.4

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -27,7 +27,7 @@ const program = new Command();
 program
   .name('flipflop2')
   .description('A CLI tool v2 for Flipflop token operations')
-  .version('1.0.3');
+  .version('1.0.4');
 
 // Add launch subcommand
 program.command('launch')
